40353d4eb9f132a2deb59dc6a5ddb4c229a46cd3
aryarmdhn <128439373+aryarmdhn@users.noreply.github.com>
2023-09-03 19:31:45 +0700

    ProductDetails.js

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -11,28 +11,29 @@ const ProductDetails = () => {
   const product = products.find((item) => {
     return item.id === parseInt(id);
   });
-  // if product is not found
+
+  // Jika produk tidak ditemukan
   if (!product) {
     return <section className="h-screen flex justify-center items-center">Harap Tunggu Ya...</section>;
   }
 
-  // destructure
+  // Destrukturisasi
   const { title, price, description, image } = product;
+
   return (
-    <section className="pt-32 pb-12 lg:py-32 h-screen flex items-center">
+    <section className="pt-8 pb-4 md:pt-12 md:pb-8 lg:pt-16 lg:pb-12 h-screen flex items-center">
       <div className="container mx-auto">
-        {/* {img and text} */}
         <div className='flex flex-col lg:flex-row items-center'>
-          {/* {image} */}
-          <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0'>
-            <img className='max-w-[200px] lg:max-w-sm' src={image} alt="" />
+          {/* Gambar */}
+          <div className='flex flex-1 justify-center items-center mb-4 lg:mb-0 mt-3'>
+            <img className='max-w-[200px] lg:max-w-sm w-full h-auto' src={image} alt={title} />
           </div>
-          {/* {text} */}
+          {/* Teks */}
           <div className='flex-1 text-center lg:text-left'>
-            <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>{title}</h1>
-            <div className='text-xl text-red-500 font-medium mb-6'>Rp. {price}</div>
-            <p className='mb-8'>{description}</p>
-            <button onClick={()=> addToCart(product, product.id)} className='bg-green-500 py-4 px-8 text-white'>Tambah Ke Keranjang</button>
+            <h1 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>{title}</h1>
+            <div className='text-xl md:text-2xl lg:text-3xl text-red-500 font-medium mb-4'>Rp. {price}</div>
+            <p className='text-sm md:text-base mb-6'>{description}</p>
+            <button onClick={() => addToCart(product, product.id)} className='bg-green-500 py-3 md:py-4 px-6 md:px-8 text-white mb-5'>Tambah Ke Keranjang</button>
           </div>
         </div>
       </div>
